f55523f6a2f6cfc92160526a042d98bf28e3518d
Emmanuel Ovando <dmkboo@gmail.com>
2017-03-28 12:56:47 -0600

    initial commit

---
+++ controllers/authentication.js
@@ -0,0 +1,43 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+  const timestamp = new Date().getTime();
+  return jwt.encode( { sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next){
+  //user has already had their email and password auth'd
+  //we just need give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = function(req, res, next){
+  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  if(!email || !password){
+    return res.status(422).send({ error: 'You must provide email and password' })
+  }
+  // See if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser){
+    if(err) { return next(err); }
+
+    // if a user with email does exist, return an Error
+    if(existingUser) {
+      return res.status(422).send({ error: 'Email is in use' })
+    }
+    //if  a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err){
+      if(err){ return next(err); }
+    });
+
+    //respond to request indicating the user was created
+    res.json({token: tokenForUser(user)});
+  });
+}
